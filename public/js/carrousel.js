a70ab1d8810f0eca8af742168039b94a01ae7f94
Zicka67 <art.book@hotmail.fr>
2024-06-30 23:25:08 +0200

    Ajout d'un menu vertical a gauche, + début du front pour ma homepage et produits.
    Modif dans le carousel

--- public/js/carrousel.js
+++ public/js/carrousel.js
@@ -7,11 +7,26 @@ function showNextImage() {
     images[currentImageIndex].classList.add('active');
 }
 
-// Fonction pour le bouton
+function showPrevImage() {
+    images[currentImageIndex].classList.remove('active');
+    // Ajout de la condition pour gérer l'index négatif
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    images[currentImageIndex].classList.add('active');
+}
+
+// Fonction pour le bouton "Next"
 function nextImage() {
     // Réinitialise le timer pour que l'image suivante s'affiche après 5 secondes après le clic
     clearInterval(autoSlideInterval);
     showNextImage();
+    autoSlideInterval = setInterval(showNextImage, 5000);
+}
+
+// Fonction pour le bouton "Prev"
+function prevImage() {
+    // Réinitialise le timer pour que l'image suivante s'affiche après 5 secondes après le clic
+    clearInterval(autoSlideInterval);
+    showPrevImage();
     autoSlideInterval = setInterval(showNextImage, 5000);
 }
 
